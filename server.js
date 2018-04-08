0d70a84c472cf94d56d0a7ae1cbd37b7d921afc6
RazielLewach <37403178+RazielLewach@users.noreply.github.com>
2018-04-08 21:05:09 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ const mysql = require('mysql');
 
 // Inicializamos la base de datos
 var con = mysql.createConnection({
-     host: "localhost",
+     host: "eu-cdbr-west-02.cleardb.net",
+     // host: "localhost",
      user: "bd0a2b4ce07342",
      // user: "admin",
-     password: "cc65b946",
+     password: "5e67cbaed168786",
      // password: "password",
      database: "heroku_8f53c8984463c5b"
      // database: "dctdb"
